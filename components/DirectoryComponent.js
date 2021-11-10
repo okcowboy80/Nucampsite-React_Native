3acf6f21625aa8af6d7b268dd9b203529caa6b1f
okcowboy80 <okcowboy80@gmail.com>
2021-11-10 12:35:23 -0600

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -6,11 +6,12 @@ function Directory(props) {
 
     const renderDirectoryItem = ({item}) => {
         return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-            />
+          <ListItem
+            title={item.name}
+            subtitle={item.description}
+            onPress={() => props.onPress(item.id)}
+            leftAvatar={{ source: require('./images/react-lake.jpg')}}
+          />
         );
     };
 
